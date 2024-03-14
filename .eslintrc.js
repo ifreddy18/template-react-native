08b5b2e83517f3a1a37c72c3fb8e0208792c1bf1
Freddy Michelena <freddy.michelena@tutenlabs.com>
2024-03-14 15:27:34 -0400

    Base template

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+	root: true,
+	extends: '@react-native',
+	semi: false,
+	tabWidth: 2,
+}
---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	arrowParens: 'avoid',
+	bracketSameLine: false,
+	bracketSpacing: true,
+	endOfLine: 'auto',
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	tabWidth: 2,
+	useTabs: true,
+	'object-curly-newline': true,
+}
